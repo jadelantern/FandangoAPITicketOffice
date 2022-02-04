ac5814f511e60593e2d01a24e1cf60f1ab5d8bd7
Jadelantern <natelee00@gmail.com>
2022-02-03 19:40:37 -0500

    Added methods to handle each type of call we would need (InTheaters, ComingSoon). Cleaned up method that was doing more than one task.

--- ImdbAPITicketOffice.Console/Program.cs
+++ ImdbAPITicketOffice.Console/Program.cs
@@ -1,7 +1,10 @@
 ﻿// IMDB API Documentation: https://imdb-api.com/api
 
 using ImdbAPITicketOffice.Lib;
+using System.Net;
 
+
+const string baseURL = "https://imdb-api.com/en/API";
 string apiKeyFilePath = Environment.CurrentDirectory + "\\imdbApiKey.txt";
 APIKeyFile apiFile = new APIKeyFile(apiKeyFilePath);
 if (string.IsNullOrEmpty(apiFile.Key))
@@ -10,5 +13,30 @@ if (string.IsNullOrEmpty(apiFile.Key))
     Console.WriteLine("Program will not run without API Key. Program will now exit.");
     return;
 }
-
 Console.WriteLine("API Key found.");
+
+string result = InTheaters(apiFile);
+//Console.WriteLine(result);
+
+
+
+string HTTPClientResult(APIKeyFile apiFile, string apiCall)
+{
+    HttpClient client = new HttpClient();
+    var request = client.GetStringAsync($"{baseURL}/{apiCall}/{apiFile.Key}");
+    request.Wait();
+
+    return request.Result;
+}
+
+string InTheaters(APIKeyFile apiFile)
+{
+    string result = HTTPClientResult(apiFile, "InTheaters");
+    return result;
+}
+
+string ComingSoon(APIKeyFile apiFile)
+{
+    string result = HTTPClientResult(apiFile, "ComingSoon");
+    return result;
+}
