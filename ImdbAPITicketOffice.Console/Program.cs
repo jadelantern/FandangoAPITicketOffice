0fa8c8416de86026cc8333e2587f29fa263c1929
Jadelantern <natelee00@gmail.com>
2022-02-05 16:03:51 -0500

    Added Imdb class, moved imdb calls into the class. Created a constructor for code we alway want to run and that needs to be set first.  Created local variable in program cs to call information in imdb class.

--- ImdbAPITicketOffice.Console/Program.cs
+++ ImdbAPITicketOffice.Console/Program.cs
@@ -4,9 +4,8 @@ using ImdbAPITicketOffice.Lib;
 using System.Net;
 
 
-const string baseURL = "https://imdb-api.com/en/API";
-string apiKeyFilePath = Environment.CurrentDirectory + "\\imdbApiKey.txt";
-APIKeyFile apiFile = new APIKeyFile(apiKeyFilePath);
+
+
 if (string.IsNullOrEmpty(apiFile.Key))
 {
     Console.WriteLine($"No API Key found. Please enter your IMDB API Key in the file located at: {apiKeyFilePath}");
@@ -15,28 +14,10 @@ if (string.IsNullOrEmpty(apiFile.Key))
 }
 Console.WriteLine("API Key found.");
 
+string apiKeyFilePath = Environment.CurrentDirectory + "\\imdbApiKey.txt";
+ImdbApi api = new ImdbApi(apiKeyFilePath);
 string result = InTheaters(apiFile);
 //Console.WriteLine(result);
 
 
 
-string HTTPClientResult(APIKeyFile apiFile, string apiCall)
-{
-    HttpClient client = new HttpClient();
-    var request = client.GetStringAsync($"{baseURL}/{apiCall}/{apiFile.Key}");
-    request.Wait();
-
-    return request.Result;
-}
-
-string InTheaters(APIKeyFile apiFile)
-{
-    string result = HTTPClientResult(apiFile, "InTheaters");
-    return result;
-}
-
-string ComingSoon(APIKeyFile apiFile)
-{
-    string result = HTTPClientResult(apiFile, "ComingSoon");
-    return result;
-}
---
+++ ImdbAPITicketOffice.Lib/ImdbApi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImdbAPITicketOffice.Lib
+{
+    public class ImdbApi
+    {
+        /// <summary>
+        /// The base url for the entire project. Used for every API call.
+        /// </summary>
+        public const string baseURL = "https://imdb-api.com/en/API";
+        public APIKeyFile apiFile;
+        
+        public ImdbApi(string apiKeyFilePath)
+        {
+            apiFile = new APIKeyFile(apiKeyFilePath);
+        }
+
+        string HTTPClientResult(APIKeyFile apiFile, string apiCall)
+        {
+            HttpClient client = new HttpClient();
+            var request = client.GetStringAsync($"{baseURL}/{apiCall}/{apiFile.Key}");
+            request.Wait();
+
+            return request.Result;
+        }
+
+        string InTheaters(APIKeyFile apiFile)
+        {
+            string result = HTTPClientResult(apiFile, "InTheaters");
+            return result;
+        }
+
+        string ComingSoon(APIKeyFile apiFile)
+        {
+            string result = HTTPClientResult(apiFile, "ComingSoon");
+            return result;
+        }
+    }
+}
